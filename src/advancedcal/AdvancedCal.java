597ea58960b8eb5a6608d5b9407382712c616a47
Vihanga Amarasinghe Z <142644936+vihanga886@users.noreply.github.com>
2024-09-21 19:52:47 +0530

    Add files via upload

---
+++ src/advancedcal/AdvancedCal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package advancedcal;
+
+/**
+ *
+ * @author USER
+ */
+public class AdvancedCal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+    }
+}
